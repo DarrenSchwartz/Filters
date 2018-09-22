3921d47d989595f736caa0047c28ef6805b5c036
LiquidSchwartz <33355526+LiquidSchwartz@users.noreply.github.com>
2018-09-22 15:49:03 -0700

    Update Greasemonkey.user.js

--- Greasemonkey.user.js
+++ Greasemonkey.user.js
@@ -1,1 +1,19 @@
+// ==UserScript==
+// @name     tinker with icons
+// @version  1
+// @grant    none
+// ==/UserScript==
+document.body.style.border = "1px solid red";
 
+var tits;
+tits = document.querySelectorAll('.topnav-icons,.icons,.icon');
+for (var i=0; i < tits.length; i++) {
+  tits[i].innerHTML = tits[i].title;
+  tits[i].style.border = "1px solid red";
+  tits[i].style.fontSize = "10px !important";
+  tits[i].style.fontColor = "blue !important";
+
+  tits[i].style.padding = "1px !important";
+
+  console.log("shitty Icons: " + i);
+}
