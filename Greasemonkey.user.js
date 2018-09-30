122e50637fbb56e92f9379abdc53917d5ad690e0
LiquidSchwartz <33355526+LiquidSchwartz@users.noreply.github.com>
2018-09-30 11:20:36 -0700

    Update Greasemonkey.user.js

--- Greasemonkey.user.js
+++ Greasemonkey.user.js
@@ -3,31 +3,24 @@
 // @version  1
 // @grant    none
 // ==/UserScript==
+
 document.body.style.border = "1px solid red";
 
 var tits;
-tits = document.querySelectorAll('.topnav-icons,.icons,.icon');
+tits = document.querySelectorAll('.topnav-icons, .w3-bar-item, .w3-button, .w3-btn, .btn-link, .btn, button, .HeaderNavlink');
 for (var i=0; i < tits.length; i++) {
-  tits[i].innerHTML = tits[i].title;
-  tits[i].style.border = "1px solid red";
-  tits[i].style.fontSize = "12px !important";
-  tits[i].style.padding = "1px !important";
-
-  console.log("shitty Icons: " + i);
-}
-
-// for github
-var ass;
-ass = document.querySelectorAll('.details-overlay,.btn-link,.dropdown,.btn,button');
-for (var i=0; i < ass.length; i++) {
-  ass[i].style.border = "1px solid red";
-
+  
+  if (tits[i].title != "") {
+      tits[i].innerHTML = tits[i].title;
+  }
+  
+  tits[i].style = "background-color: #ffffcc !important; color:black !important; padding:2px !important; font-size:12px !important; width:auto !important; border:1px solid red !important";
+  console.log(tits[i].title);
 }
 
 var dick;
-dick = document.querySelectorAll('.btn-octicon');
+dick = document.querySelectorAll('.btn-octicon, button, .tooltipped');
 for (var i=0; i < dick.length; i++) {
   dick[i].innerHTML = dick[i].getAttribute("aria-label");
-  dick[i].style.border = "1px solid red";
-
+  dick[i].style = "background-color: #ffffcc !important; color:black !important; padding:2px !important; font-size:12px !important; width:auto !important; border:1px solid red !important";
 }
